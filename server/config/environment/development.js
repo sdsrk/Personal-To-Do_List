729c13173476a34b6b5e3beb46f7aee2ced675eb
shardul <channeli@outlook.com>
2016-02-13 19:32:06 +0530

    make it better

---
+++ server/config/environment/development.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/todo-dev'
+  },
+
+  seedDB: true,
+  google: {
+    clientID: '177085260127-viq2qum7am03d5qn5pgst53a44dv6l0b.apps.googleusercontent.com',
+    clientSecret: '1SzR0QUZXHDphk6Un3Su8bNw',
+    //callbackURL: 'http://hrmsplus.accolite.com/auth/google/callback'
+    callbackURL: 'http://localhost:9000/auth/google/callback'
+  }
+};
